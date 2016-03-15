1c5ba346422e1e24842c4f05895c7e70792d5364
Joe Hohertz <jhohertz@gmail.com>
2016-03-15 17:55:40 -0400

    bump default spark to 1.6.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,13 +1,13 @@
 
 default['apache_spark']['install_mode'] = 'tarball'
 
-default['apache_spark']['download_url'] = 'http://d3kbcqa49mib13.cloudfront.net/spark-1.6.0-bin-hadoop2.6.tgz'
+default['apache_spark']['download_url'] = 'http://apache.mirror.rafal.ca/spark/spark-1.6.1/spark-1.6.1-bin-hadoop2.6.tgz'
 # The SHA256 checksum of the downloaded tarball.
-default['apache_spark']['checksum'] = '439fe7793e0725492d3d36448adcd1db38f438dd1392bffd556b58bb9a3a2601'
+default['apache_spark']['checksum'] = '09f3b50676abc9b3d1895773d18976953ee76945afa72fa57e6473ce4e215970'
 
 # These options are used with the "package" installation mode.
 default['apache_spark']['pkg_name'] = 'spark'
-default['apache_spark']['pkg_version'] = '1.6.0'
+default['apache_spark']['pkg_version'] = '1.6.1'
 
 # For setting ulimits for spark user
 node.default["ulimit"]["users"][node["apache_spark"]["user"]]["filehandle_limit"] = 65536
